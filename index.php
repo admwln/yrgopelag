72080e4800072772b16e3f91909c5400a0f32007
Adam Welin <welinadam@users.noreply.github.com>
2023-12-29 18:09:24 +0100

    Images added, success-style.css created

--- index.php
+++ index.php
@@ -133,7 +133,10 @@ $selectedRoomId = (isset($_SESSION['roomId'])) ? $_SESSION['roomId'] : 1;
 
         </form>
     </main>
-    <footer></footer>
+    <footer>
+        <!-- <i class="fa-brands fa-creative-commons"></i>&nbsp;
+        Adam Welin 2024 -->
+    </footer>
     <script src="js/script.js"></script>
     <script src="js/slider.js"></script>
 </body>
